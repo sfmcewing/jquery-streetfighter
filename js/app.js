3b4ba83417dbb8bee9c94449c146a34a5aafef3a
Sarah <sfmcewing@gmail.com>
2015-10-15 15:03:00 -0700

    Add mouseup and mousedown events to ryu's position

--- js/app.js
+++ js/app.js
@@ -6,6 +6,17 @@ $(document).ready(function() {
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
-	});
-
+	})
+	.mousedown(function() {
+    // play hadouken sound
+    	$('.ryu-ready').hide();
+    	$('.ryu-throwing').show();
+    	$('.hadouken').show();
+    // show hadouken and animate it to the right of the screen
+  	})
+  	.mouseup(function() {
+    // ryu goes back to his ready position
+    	$('.ryu-throwing').hide();
+    	$('.ryu-ready').show();
+  });
 });
