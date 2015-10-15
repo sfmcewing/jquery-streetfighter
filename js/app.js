4af5b7bb788586002acc144ee2578af6e5928364
Sarah <sfmcewing@gmail.com>
2015-10-15 15:15:30 -0700

    Add hadouken sound effect

--- js/app.js
+++ js/app.js
@@ -9,10 +9,21 @@ $(document).ready(function() {
 	})
 	.mousedown(function() {
     // play hadouken sound
+    	playHadouken();
     	$('.ryu-ready').hide();
     	$('.ryu-throwing').show();
-    	$('.hadouken').show();
-    // show hadouken and animate it to the right of the screen
+    	// finish method completes all currently running animations on an event
+    	$('.hadouken').finish().show().animate(
+    		//animate so its left value is 1020px (500px to the right)
+    		{'left': '1020px'},
+    		// 500 milliseconds is how long the animation will take
+    		500,
+    		// will hide hadouken when the animation finishes, and set its left property back to original 520px
+    		function() {
+    			$(this).hide();
+    			$(this).css('left', '520px');
+    		}
+    		);
   	})
   	.mouseup(function() {
     // ryu goes back to his ready position
@@ -20,3 +31,9 @@ $(document).ready(function() {
     	$('.ryu-ready').show();
   });
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
